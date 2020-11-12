5cbd1a2ab6ac5d944d19bd0d4f09460a33d87b5a
Haci Karahasanoglu <hacikho01@gmail.com>
2020-11-12 18:31:29 +0000

    Combine two arrays into a list, sort the list, convert list to array and print

--- main.cs
+++ main.cs
@@ -1,7 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    //Console.WriteLine ("Hello World");
+    int[] x = new int[4]{0,3,4,31};
+    int[] y = new int[3]{4,6,30};
+    mergerSortedArrays(x,y);
+  }
+
+  public static void mergerSortedArrays(int[] arr1, int[] arr2){
+    List<int> result = new List<int>(); 
+
+    for(int i = 0; i < arr1.Length; i++){
+      result.Add(arr1[i]);
+    }
+
+     for(int j = 0; j < arr2.Length; j++){
+      result.Add(arr2[j]);
+    }
+
+    result.Sort();
+
+    int[] array = result.ToArray();
+
+    for(int a = 0; a< array.Length; a++){
+      Console.WriteLine(array[a]);
+    }
   }
 }
